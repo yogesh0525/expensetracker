1e31f9bf09f366273e5fe551d8b26f1ef9c5c104
yogesh0525 <eyogeshkumar123@gmail.com>
2026-06-25 08:28:30 +0530

    Update url

--- expense-tracker-backend-main/server.js
+++ expense-tracker-backend-main/server.js
@@ -15,7 +15,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://selavapathuko.vercel.app/"
+      "https://selavapathuko.vercel.app"
     ],
     credentials: true,
   })
